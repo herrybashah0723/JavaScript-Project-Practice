3d13bd7fa28f1b0e3f15f73dbb712b4d288c6703
Humza <109144839+humzaco@users.noreply.github.com>
2023-03-04 10:09:36 +0500

    Funcaions

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 // ----------while lopp Game
 
-const maxValue = 10;
-const randomNumber = Math.floor(Math.random()*maxValue) + 1;
-let status = false;
+// const maxValue = 10;
+// const randomNumber = Math.floor(Math.random()*maxValue) + 1;
+// let status = false;
 
-while (!status) {
-    let userInput = prompt("Guess a number between 1 and " + maxValue)
-    userInput = Number(userInput);
-    if (userInput === randomNumber){
-        status = true;
-        console.log("You won! The number was " + randomNumber);
-    } else if ( userInput > randomNumber ) {
-        console.log("Sorry, your guess is too high.");
-    } else {
-        console.log("Sorry, your guess was too low.");
-    }
+// while (!status) {
+//     let userInput = prompt("Guess a number between 1 and " + maxValue)
+//     userInput = Number(userInput);
+//     if (userInput === randomNumber){
+//         status = true;
+//         console.log("You won! The number was " + randomNumber);
+//     } else if ( userInput > randomNumber ) {
+//         console.log("Sorry, your guess is too high.");
+//     } else {
+//         console.log("Sorry, your guess was too low.");
+//     }
+// }
+
+
+
+
+// -------------Functions
+
+function coffeeOrder(drink) {
+    return `your ${drink} is on its way!`
 }
+
+const order = coffeeOrder('Latte')
+console.log(order);
